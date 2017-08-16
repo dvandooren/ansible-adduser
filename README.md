f32a244f112a150c362d46c1f5f6df69dbabc2af
dvandoor <dvandoor AT cisco Dot com>
2017-08-16 08:06:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ansible Role - Add Users
+
+This is just a quick playbook to add users with their passwords and/or their ssh key to a set of serves. This playbook runs are root.
+
+## Step 1: Configure envs/<files>
+- Create a `envs/<project>.hosts` file based on `env/sample.hosts` file. Add your servers and IPs to the `[servers]` group.
+- Create a `envs/<project>.config` file based on the `envs/sample.config` file to define the needed users and credentials.
+
+## Step 2: Run playbook
+The first playbook `add_users.yaml` will add the configured users to the server
+
+```Shell
+./scripts/env-ansible.sh <project> add_users.yaml --ask-pass
+```
---
+++ scripts/env-ansible.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# Wrapper tool to ease the use of ansible with multiple variable files for different projects
+#
+# This script should be run from the main project dir (ie. the dir that has the playbooks and the envs dir under it)
+#
+# This script is based on the Apache Licensed openstak-ansible script created and (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH}"
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+CBLK='\033[0;30m'
+CRED='\033[0;31m'
+CGRN='\033[0;32m'
+CYLW='\033[0;33m'
+CBLU='\033[0;34m'
+CPUR='\033[0;35m'
+CCYN='\033[0;36m'
+CGRY='\033[0;37m'
+CEND='\033[0;0m'
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <PROJECT>"
+	echo
+	echo "You must specify the project name to load the inventory and variable files for."
+	echo "     ie. $0 sample"
+	echo
+	echo "The variable files must be named <PROJECT>.config and <PROJECT>.secret"
+	echo "The inventory file must be named <PROJECT>.hosts"
+	exit 1
+fi
+
+function info() {
+    echo -e "${CPUR}${*}${CEND}"
+}
+
+# Discover the variable files.
+#VARS="-e @envs/$1.config -e @envs/$1.secret"
+INV="-i envs/$1.hosts"
+CONFIGFILES=`find envs -name $1.* -type f | grep -v hosts`
+
+for FILE in $CONFIGFILES
+do
+	VARS+="-e @$FILE "
+done
+
+# Shift the command line varibles to pass along to ansible-playbook
+shift
+
+# Provide information on the discovered variables.
+info "Variable files: \"${VARS}\""
+info "Inventory file: \"${INV}\""
+
+# Run the ansible playbook command.
+$(which time) "$(which ansible-playbook)" ${INV} ${VARS} "$@"
